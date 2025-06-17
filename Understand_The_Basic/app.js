c69cad896da39b1b7f14ef6d462c39327b879d7c
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2025-06-17 10:48:39 +0000

    Learn about the response

--- Understand_The_Basic/app.js
+++ Understand_The_Basic/app.js
@@ -3,6 +3,12 @@ const http = require('http');
 const server = http.createServer((req,res) => {
     console.log(req.headers, req.url, req.method);
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body>Hello From My Node Server</body>')
+    res.write('</html>')
+    res.end();
 })
 
 server.listen(3000);
