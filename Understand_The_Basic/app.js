7994e5251783660701855748a3f66b158c91390f
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2025-06-17 10:35:56 +0000

    Learn about the request

--- Understand_The_Basic/app.js
+++ Understand_The_Basic/app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log(res);
+    console.log(req.headers, req.url, req.method);
+    // process.exit();
 })
 
 server.listen(3000);
