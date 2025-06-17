9456154f42d883c693dadbf681cf0c5b880ed875
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2025-06-17 11:38:20 +0000

    Learn how to read and parse body from request

--- Understand_The_Basic/app.js
+++ Understand_The_Basic/app.js
@@ -1,8 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-    console.log(req.headers, req.url, req.method);
-    // process.exit();
+ const url = req.url;
+ const method = req.method;
+
+ if(url === "/"){
+   res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></input></form></body>')
+    res.write('</html>')
+    return res.end();
+ }
+
+ if(url === "/message" && method === "POST"){
+    const body = [];
+    req.on('data' , (chunk) => {
+        body.push(chunk);
+    })
+    req.on('end', () => {
+        const parseBody = Buffer.concat(body).toString();
+        const message = parseBody.split("=")[1];
+        fs.writeFileSync('message.txt',message);
+    })
+
+    res.statusCode = 302;
+    res.setHeader('Location', "/")
+    return res.end();
+ }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
